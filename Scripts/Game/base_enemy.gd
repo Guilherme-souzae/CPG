448cbf2bb69f6299a4cac86b5aa4d10f64b23c66
Guilherme-souzae <guiemergente@gmail.com>
2025-05-18 03:39:27 -0300

    inimigos saborosos

--- Scripts/Game/base_enemy.gd
+++ Scripts/Game/base_enemy.gd
@@ -1,14 +1,17 @@
 extends CharacterBody2D
 
+@export var enemySprite: Array[Texture2D]
 @export var enemyData: Resource = null
 @export var speed = 100
 
 # Variáveis de movimento
 var target = null
 var can_see_player = false
+var last_direction = 0  # 0=baixo, 1=direita, 2=esquerda, 3=cima
 
 func _ready():
 	# Conecta os sinais das áreas
+	$Sprite2D.texture = enemySprite[0]
 	$DetectionArea.connect("body_entered", Callable(self, "_on_DetectionArea_body_entered"))
 	$DetectionArea.connect("body_exited", Callable(self, "_on_DetectionArea_body_exited"))
 	$CombatArea.connect("body_entered", Callable(self, "_on_CombatArea_body_entered"))
@@ -23,6 +26,9 @@ func _physics_process(delta):
 		# Move o inimigo
 		position += direction * speed * delta
 		
+		# Determina a direção predominante para atualizar o sprite
+		update_sprite_direction(direction)
+		
 		# Atualiza o RayCast para apontar para o jogador
 		$RayCast2D.target_position = to_local(target.global_position)
 		$RayCast2D.force_raycast_update()
@@ -32,7 +38,33 @@ func _physics_process(delta):
 			var collider = $RayCast2D.get_collider()
 			if not collider.is_in_group("player"):
 				can_see_player = false
-				# Aqui você pode adicionar comportamento quando perde a visão do jogador
+
+func update_sprite_direction(direction: Vector2):
+	# Determina a direção predominante
+	if abs(direction.x) > abs(direction.y):
+		# Movimento horizontal predominante
+		if direction.x > 0:
+			# Direita
+			if last_direction != 1:
+				$Sprite2D.texture = enemySprite[1]
+				last_direction = 1
+		else:
+			# Esquerda
+			if last_direction != 2:
+				$Sprite2D.texture = enemySprite[2]
+				last_direction = 2
+	else:
+		# Movimento vertical predominante
+		if direction.y > 0:
+			# Baixo
+			if last_direction != 0:
+				$Sprite2D.texture = enemySprite[0]
+				last_direction = 0
+		else:
+			# Cima
+			if last_direction != 3:
+				$Sprite2D.texture = enemySprite[3]
+				last_direction = 3
 
 func _on_DetectionArea_body_entered(body):
 	if body.is_in_group("player"):
@@ -62,5 +94,6 @@ func _on_combat_ran():
 	await $RunTimer.timeout
 	get_node("CombatArea/CollisionShape2D").disabled = false
 	get_node("DetectionArea/CollisionShape2D").disabled = false
+
 func _on_combat_won():
 	queue_free()
